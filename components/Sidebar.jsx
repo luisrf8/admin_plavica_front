585802a80f8f5e5715f2116110f308c06d4d2bda
Luis Rojas <lrojas@tecfin212.com>
2024-02-05 17:52:08 -0400

    actualizacion

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,7 +1,19 @@
 // Sidebar.jsx
-// import { useState } from 'react';
+import { useEffect } from 'react';
+import api from '../lib/axios'
 
 const Sidebar = ({ isOpen, toggleSidebar }) => {
+  useEffect(() => {
+    api
+      .get(`/orders`)
+      .then((response) => {
+        console.log("resp", response);
+        // setCompetitors(response.data.response);
+      })
+      .catch((error) => {
+        console.log("errr", error);
+      });
+  }, []);
   return (
     <aside className={`fixed ${isOpen ? 'sidebar open' : 'sidebar'}`}>
       {/* Add links to different sections */}
